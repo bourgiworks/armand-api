50b8b08a50d11dd5f78045929df1853f6ae72311
bourgiworks <bourgiworks@gmail.com>
2022-05-11 15:14:01 +0200

    token commit

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Headers', 'auth-token');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
