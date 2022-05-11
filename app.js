67e5324ad7712cc90da502d35babf4443de7ddd9
bourgiworks <bourgiworks@gmail.com>
2022-05-11 15:48:33 +0200

    token commit

--- app.js
+++ app.js
@@ -51,10 +51,10 @@ const corsOpts = {
   app.use(cors(corsOpts));
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Access-Control-Request-Method, Access-Control-Request-Headers');
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 });
 
